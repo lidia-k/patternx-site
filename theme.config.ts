dfd91b891fc9bbf54504cfbdcba9f330844443ed
win <khacnha.it@gmail.com>
2024-07-20 17:04:03 +0700

    chore: add favvico and optimize theme

--- theme.config.ts
+++ theme.config.ts
@@ -2,44 +2,11 @@ import type { CustomFlowbiteTheme } from "flowbite-react";
 
 const customTheme: CustomFlowbiteTheme = {
   navbar: {
-    root: {
-      base: "bg-white px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4",
-      rounded: {
-        on: "rounded",
-        off: "",
-      },
-      bordered: {
-        on: "border",
-        off: "",
-      },
-      inner: {
-        base: "mx-auto flex flex-wrap items-center justify-between",
-        fluid: {
-          on: "",
-          off: "container",
-        },
-      },
-    },
-    brand: {
-      base: "flex items-center",
-    },
-    collapse: {
-      base: "w-full md:block md:w-auto",
-      list: "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium",
-      hidden: {
-        on: "hidden",
-        off: "",
-      },
-    },
     link: {
       base: "block py-2 pl-3 pr-4 md:p-0",
       active: {
         on: "bg-blue-600 text-white dark:text-white md:bg-transparent md:text-blue-600",
         off: "border-b border-gray-100 text-gray-800 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white",
-      },
-      disabled: {
-        on: "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
-        off: "",
       },
     },
     toggle: {
